4f72033a57d845c02678a733e313364b8fe8622e
nurwanto <nurwanto16@gmail.com>
2018-03-24 17:38:59 +0700

    update

--- src/main/java/id/noeandfriends/eproc/controller/web/DashboardController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/DashboardController.java
@@ -1,15 +1,39 @@
 package id.noeandfriends.eproc.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import id.noeandfriends.eproc.model.Login;
+import id.noeandfriends.eproc.model.User;
+import id.noeandfriends.eproc.repository.ProcurementRepository;
+import id.noeandfriends.eproc.repository.ProposalRepository;
+import id.noeandfriends.eproc.repository.UserRepository;
+
 @Controller
-@RequestMapping("/dashboard")
+@RequestMapping("/{user_id}/dashboard")
 public class DashboardController {
 	
+	@Autowired
+	ProcurementRepository procurementRepository;
+	
+	@Autowired
+	ProposalRepository proposalRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@RequestMapping("")
-	public String index(Model model) {
+	public String index(Model model, @PathVariable String user_id) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		User user = userRepository.findById(user_id).get();
+		model.addAttribute("user", user);
+		model.addAttribute("user_json", mapper.writeValueAsString(user));
 		return "index";
 	}
 	
--- src/main/java/id/noeandfriends/eproc/controller/web/LoginController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/LoginController.java
@@ -5,14 +5,20 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import id.noeandfriends.eproc.controller.AbstractContractorController;
+import id.noeandfriends.eproc.model.Login;
+import id.noeandfriends.eproc.model.User;
+
 @Controller
 @RequestMapping("/login")
-public class LoginController {
+public class LoginController extends AbstractContractorController{
 	@RequestMapping("")
 	public String index(Model model) {
 		return "/pages/examples/login";
@@ -22,7 +28,13 @@ public class LoginController {
 	public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		System.out.println("email "+email);
-		response.sendRedirect("/dashboard");
+		
+		Login login = new Login();
+		
+		login.setEmail(email);
+		login.setPassword(password);
+		
+		User user = login(login).getBody();
+		response.sendRedirect("/" + user.getId() + "/dashboard");
 	}
 }
