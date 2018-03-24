d3496cf2186a0908db9753d0be50215b20a47a7f
nurwanto <nurwanto16@gmail.com>
2018-03-24 23:35:28 +0700

    update

--- src/main/java/id/noeandfriends/eproc/controller/web/ProposalController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/ProposalController.java
@@ -1,7 +1,13 @@
 package id.noeandfriends.eproc.controller.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import id.noeandfriends.eproc.controller.rest.ContractorRestController;
@@ -14,12 +20,22 @@ public class ProposalController extends ContractorRestController{
 	public String index(Model model) {
 		return "pages/tables/proposal";
 	}
-	@RequestMapping("/add")
-	public String form_create_proposal(Model model) {
+	@PostMapping("/add")
+	public String form_create_proposal(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String judul = request.getParameter("proposalJudul");
+		String deskripsi = request.getParameter("proposalDeskripsi");
+		String biaya = request.getParameter("proposalBiaya");
+		String durasi = request.getParameter("proposalDurasi");
+		String File = request.getParameter("proposalFile"); 
 		return "pages/tables/add_proposal";
 	}
-	@RequestMapping("/edit")
-	public String form_edit_proposal(Model model) {
+	@PostMapping("/edit")
+	public String form_edit_proposal(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String judul = request.getParameter("proposalJudul");
+		String deskripsi = request.getParameter("proposalDeskripsi");
+		String biaya = request.getParameter("proposalBiaya");
+		String durasi = request.getParameter("proposalDurasi");
+		String File = request.getParameter("proposalFile"); 
 		return "pages/tables/edit_proposal";
 	}
 	
