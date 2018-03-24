0db4015fa4f89d8a7c0e827704d97fca111ddc00
nurwanto <nurwanto16@gmail.com>
2018-03-24 18:05:33 +0700

    update

--- src/main/java/id/noeandfriends/eproc/controller/web/SettingController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/SettingController.java
@@ -1,8 +1,17 @@
 package id.noeandfriends.eproc.controller.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import id.noeandfriends.eproc.model.Login;
+import id.noeandfriends.eproc.model.User;
 
 @Controller
 @RequestMapping("/setting")
@@ -11,5 +20,10 @@ public class SettingController {
 	@RequestMapping("")
 	public String index(Model model) {
 		return "pages/UI/general";
+	}
+	
+	@PostMapping("/create_account")
+	public void index(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.sendRedirect("/setting");
 	}
 }
