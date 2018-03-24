6bc3883e0568abd41b98814944e59a4b82af4ab6
Andre Pratama <andre.pratama@profisien.co.id>
2018-03-24 23:33:01 +0700

    benerin error

--- src/main/java/id/noeandfriends/eproc/EprocApplication.java
+++ src/main/java/id/noeandfriends/eproc/EprocApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan({
+		"id.noeandfriends.eproc.configuration", 
+		"id.noeandfriends.eproc.controller.rest", 
+		"id.noeandfriends.eproc.controller.web"})
 
 @SpringBootApplication
 public class EprocApplication extends SpringBootServletInitializer {
