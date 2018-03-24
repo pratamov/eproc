1cce7cd8201a4998f58c4bb20c580d17dd4c0847
nurwanto <nurwanto16@gmail.com>
2018-03-24 23:15:30 +0700

    update

--- src/main/java/id/noeandfriends/eproc/controller/web/SettingController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/SettingController.java
@@ -23,21 +23,30 @@ public class SettingController extends ContractorRestController{
 	
 	@PostMapping("/create_account")
 	public void form_create_account(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String nik = request.getParameter("ca_nik");
+		String name = request.getParameter("ca_name");
+		String tgl_lahir = request.getParameter("ca_tanggal_lahir");
+		String ibu_name = request.getParameter("ca_ibu");
+		String amount = request.getParameter("ca_amount");
 		response.sendRedirect("/setting");
 	}
 	
 	@PostMapping("/exisiting_account")
 	public void form_existing_account(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String no_rek = request.getParameter("ea_no_rek");
 		response.sendRedirect("/setting");
 	}
 	
 	@PostMapping("/virtual_account")
 	public void form_virtual_account(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String kode_institusi = request.getParameter("va_institusi");
 		response.sendRedirect("/setting");
 	}
 	
 	@PostMapping("/transfer")
 	public void form_transfer(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String norek_tujuan = request.getParameter("tf_no_rek_tujuan");
+		String amount = request.getParameter("tf_amount");
 		response.sendRedirect("/setting");
 	}
 }
