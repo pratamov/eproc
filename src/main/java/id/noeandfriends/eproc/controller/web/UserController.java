d19984df8bcedcc36a05ef6ead7c164d838180e9
nurwanto <nurwanto16@gmail.com>
2018-03-24 18:22:30 +0700

    update

--- src/main/java/id/noeandfriends/eproc/controller/web/UserController.java
+++ src/main/java/id/noeandfriends/eproc/controller/web/UserController.java
@@ -1,7 +1,13 @@
 package id.noeandfriends.eproc.controller.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +16,9 @@ public class UserController {
 	@RequestMapping("")
 	public String index(Model model) {
 		return "pages/examples/profile";
+	}
+	@PostMapping("/update")
+	public void form_update(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.sendRedirect("/user");
 	}
 }
